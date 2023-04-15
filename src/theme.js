f53f14c9369f335945627a459bf69154b3b2d144
unknown <navendra@tiobu.com>
2023-04-15 18:13:21 +0530

    Added background color and image

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export const themeSettings = () => {
+  return {
+    palette: {
+      background: {
+        alt: '#1F2833',
+        default: '#0B0C10'
+      },
+      gray: '#C5C6C7',
+      cyan: '#66FCF1',
+      blueishGreen: '#46A29E',
+      black: '#17252A',
+      darkGreen: '#2B7A78',
+      lightGreen: '#3AAFA9',
+      lightBlue: '#DEF2F1',
+      white: '#FEFFFF'
+    }
+  }
+}
